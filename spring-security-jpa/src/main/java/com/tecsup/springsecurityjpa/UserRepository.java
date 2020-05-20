a814a4496253ff805d7764d18fb8d69d33d02a5e
Juan-Carlos123 <juan.romani@tecsup.edu.pe>
2020-05-20 15:08:01 -0500

    primer commit

---
+++ spring-security-jpa/src/main/java/com/tecsup/springsecurityjpa/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tecsup.springsecurityjpa;
+
+import com.tecsup.springsecurityjpa.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
